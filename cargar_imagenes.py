90fcbfc5fcc85688de31c64110192748053251c1
Fher95 <luisfgomezg@unicauca.edu.co>
2020-10-27 10:10:49 -0500

    refinacion de la extracción HOG

--- cargar_imagenes.py
+++ cargar_imagenes.py
@@ -1,11 +1,12 @@
 import os
-from skimage.io import imread, imshow
+from skimage.io import imread #, imshow
 from skimage.transform import resize
 from skimage.feature import hog
-from skimage import exposure
-import matplotlib.pyplot as plt
-from scipy.spatial import distance
-from skimage.color import rgb2hsv
+#from skimage import exposure
+#from scipy.spatial import distance
+#from skimage.color import rgb2hsv
+from skimage.color import rgb2gray, gray2rgb
+from skimage import filters
 
 class Imagen:
 
@@ -25,12 +26,19 @@ arrImagenes = []
 contenido = os.listdir('/home/fher/Documentos/Curso CIBR/CIBR-Aves/gorrion')
 for ruta_imagen in contenido:
     img = imread('gorrion/'+ruta_imagen)    
-    resized_img = resize(img, (dimy,dimx))
+    
+    #Aplicamos el filtro Edge Roberts para quitar un poco las texturas de las ramas    
+    imgGray = rgb2gray(img)
+    edge_roberts = filters.roberts(imgGray)
+    #Se reconfigura el tamaño para tener un vector de la misma dimension para todas las imagenes
+    resized_img = resize(edge_roberts, (dimy,dimx))
     # Extracción de la caracteristica HOG
-    fd, hog_image = hog(resized_img, orientations=nOrients, pixels_per_cell=(nPixels, nPixels), 
+    fd, hog_image = hog(gray2rgb(resized_img), orientations=nOrients, pixels_per_cell=(nPixels, nPixels), 
                     cells_per_block=(nCells, nCells), visualize=True, multichannel=True)
+    
+    
     #Creación del objeto imagen para guardar el nombre y los vectores de caracteristicas
     imagen_actual = Imagen(ruta_imagen,fd)
-    
+    #Se agrega al arreglo de caracteristicas de cada imagen
     arrImagenes.append(imagen_actual)    
     
